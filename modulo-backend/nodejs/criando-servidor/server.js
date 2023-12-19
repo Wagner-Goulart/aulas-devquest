c56b2761f1029289ebca517e1b686112e4f65058
Wagner <wagnergoulart0@gmail.com>
2023-12-18 22:55:34 -0300

    ADD: Aula servidor
    Signed-off-by: Wagner <wagnergoulart0@gmail.com>

---
+++ modulo-backend/nodejs/criando-servidor/server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+   res.setHeader('Content-Type', 'text/plain');
+ 
+    if (req.url === '/') {
+       res.statusCode = 200;
+       res.end('Home page');
+   } else if (req.url === '/sobre') {
+       res.statusCode = 200;
+       res.end('About page');
+   } else {
+       res.statusCode = 404;
+       res.end('Page not found');
+   }
+});
+
+server.listen(3000, () => {
+ console.log(`Servidor em execução em http://localhost:3000/`);
+});
